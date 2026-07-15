af80d3ba8f4d67d57d1055f411931b144f7535a0
Randy <rmbrown119@gmail.com>
2026-07-14 22:34:59 -0700

    chore: update Deno imports to npm-style paths

--- tests/geocoder.test.ts
+++ tests/geocoder.test.ts
@@ -3,7 +3,7 @@
 import {
   assertEquals,
   assertExists,
-} from "https://deno.land/std@0.208.0/assert/mod.ts";
+} from "@std/assert";
 
 const censusFixture = JSON.parse(
   await Deno.readTextFile(
